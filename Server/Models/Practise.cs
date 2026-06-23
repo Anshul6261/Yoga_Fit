5f19ed25df9ad318635df9226444e204da6cf331
Anshul Khateek <118629669+Anshul6261@users.noreply.github.com>
2026-06-23 10:59:53 +0530

    Update Practise.cs

--- Server/Models/Practise.cs
+++ Server/Models/Practise.cs
@@ -1,153 +1,113 @@
-    import React, {useState} from 'react'
+Practice_Project_Neo_players_App_AD
  
-    const ContactForm = () => {
+reactapp/src/App.js:
  
-        const [formData, setformData]= useState(
-            {
-                firstName: '',
-                lastName: '',
-                age: '',
-                email: '',
-                phone: '',
-                address: '',
-                pincode: '',
-                message: ''
-            }
-        )
-        const [errors, setError]=useState({});
-        const [isSubmitted, setIsSubmitted]=useState(false)
+import React from 'react'
+import Welcome from './components/Welcome'
  
-        const handleChange= (e)=>{
-            const {name, value}= e.target
-            setformData({...formData, [name]: value})
-        }
+function App() {
+  return (
+    <div>
+      <h1>Neo Players</h1>
+      <h2>Top Students List</h2>
+      <Welcome name="Sara"/>
+    </div>
+  )
+}
  
-        const validateForm=()=>{
-            const errors={}
- 
-            if(!formData.firstName.trim()){
-                errors.firstName="First name is required"
-            }
- 
-            if(!formData.lastName.trim()){
-                errors.lastName="Last name is required"
-            }
- 
-            if(!formData.age.trim()){
-                errors.age="Age is required"
-            }
-            else if(isNaN(formData.age)|| formData.age<=0){
-                errors.age="Invalid age"
-            }
- 
-            if(!formData.email.trim()){
-                errors.email="Email is required"
-            }
-            else if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)){
-                errors.email="Invalid email format"
-            }
- 
-            if(!formData.phone.trim()){
-                errors.phone="Phone number is required"
-            }
-            else if(!/^\d{10}$/.test(formData.phone)){
-                errors.phone="Invalid phone number"
-            }
- 
-            if(!formData.address.trim()){
-                errors.address="Address is required"
-            }
- 
-            if(!formData.pincode.trim()){
-                errors.pincode="Pincode is required"
-            }
-            else if(!/^\d{6}$/.test(formData.pincode)){
-                errors.pincode="Invalid pincode"
-            }
- 
-            if(!formData.message.trim()){
-                errors.message="Message is required"
-            }
-            return errors
-        }
- 
-        const handleSubmit=(e)=>{
-            e.preventDefault()
-            const valErrors= validateForm()
-            setError(valErrors)
- 
-            if(Object.keys(valErrors).length===0){
-                setIsSubmitted(true)
-                console.log("Form submitted successfully", formData)
-                setformData({
-                firstName: '',
-                lastName: '',
-                age: '',
-                email: '',
-                phone: '',
-                address: '',
-                pincode: '',
-                message: ''
-                })
- 
-            }
-                setTimeout(()=>{
-                    setIsSubmitted(false)
-                }, 3000 )
-           
-        }
+export default App
  
  
+reactapp/src/components/Welcome.jsx :
+ 
+ 
+import React from 'react'
+ 
+const Welcome = (props) => {
+  return (
+    <div>
+        <h2>{props.name}</h2>
+    </div>
+  )
+}
+ 
+export default Welcome
+ 
+Practice_Project_Neo_Quote_App_Structure_AD
+ 
+reactapp/src/App.js:
+ 
+import React, { Component } from 'react'
+import Display from './components/display'
+import Footer from './components/footer'
+import Header from './components/header'
+ 
+export class App extends Component {
+  render() {
     return (
-        <div>
-            <h2>Contact Form</h2>
-            {isSubmitted && <p>Form submitted successfully!</p>}
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="firstName">First Name:</label>
-                    <input type="text" id="firstName" name="firstName" value={formData.firstName} onChange={handleChange}/>
-                    {errors.firstName && <p>{errors.firstName}</p>}
-                </div>
-                <div>
-                    <label htmlFor="lastName">Last Name:</label>
-                    <input type="text" id="lastName" name="lastName" value={formData.lastName} onChange={handleChange}/>
-                    {errors.lastName && <p>{errors.lastName}</p>}
-                </div>
-                <div>
-                    <label htmlFor="age">Age:</label>
-                    <input type="text" id="age" name="age" value={formData.age} onChange={handleChange}/>
-                    {errors.age && <p>{errors.age}</p>}
-                </div>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input type="text" id="email" name="email" value={formData.email} onChange={handleChange}/>
-                    {errors.email && <p>{errors.email}</p>}
-                </div>
-                <div>
-                    <label htmlFor="phone">Phone:</label>
-                    <input type="text" id="phone" name="phone" value={formData.phone} onChange={handleChange}/>
-                    {errors.phone && <p>{errors.phone}</p>}
-                </div>
-                <div>
-                    <label htmlFor="address">Address:</label>
-                    <input type="text" id="address" name="address" value={formData.address} onChange={handleChange}/>
-                    {errors.address && <p>{errors.address}</p>}
-                </div>
-                <div>
-                    <label htmlFor="pincode">Pincode:</label>
-                    <input type="text" id="pincode" name="pincode" value={formData.pincode} onChange={handleChange}/>
-                    {errors.pincode && <p>{errors.pincode}</p>}
-                </div>
-                <div>
-                    <label htmlFor="message">Message:</label>
-                    <input type="text" id="message" name="message" value={formData.message} onChange={handleChange}/>
-                    {errors.message && <p>{errors.message}</p>}
-                </div>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
+      <div>
+      <Header/>
+      <Display/>
+      <Footer/>
+      </div>
     )
-    }
+  }
+}
  
-    export default ContactForm
+export default App
+ 
+reactapp/src/components/display.jsx :
+ 
+ 
+import React, { Component } from 'react'
+ 
+export class display extends Component {
+  render() {
+    return (
+      <div>
+      <p>Quote Loading ...</p>
+      <img src="https://icons8.com/preloaders/preloaders/1494/Spinner-2.gif" alt="Placeholder"></img>
+      </div>
+     
+    )
+  }
+}
+ 
+export default display
+ 
+reactapp/src/components/footer.jsx :
+ 
+ 
+import React from 'react'
+ 
+function footer() {
+    const year = new Date().getFullYear();
+  return (
+    <div>
+    <p>Copyright {year} Quote App. All rights reserved.</p>
+    </div>
+  )
+}
+ 
+export default footer
+ 
+reactapp/src/components/header.jsx :
+ 
+ 
+import React from 'react'
+ 
+function header() {
+  return (
+    <div>
+    <h1>Neo Daily Quote App</h1>
+    <nav>
+    <ul>All Quotes</ul>
+    <ul>Add Quote</ul>
+    <ul>Favorite Quotes</ul>
+    </nav>
+    </div>
+  )
+}
+ 
+export default header
  
