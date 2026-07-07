f23b62a200c06a328232eb4fe68ed90cc979a970
Anshul Khateek <118629669+Anshul6261@users.noreply.github.com>
2026-07-07 11:30:21 +0530

    S_Practise.cs

--- Server/Models/S_Practise.cs
+++ Server/Models/S_Practise.cs
@@ -1,281 +1,330 @@
-navbar.jsx
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using dotnetapp.Models;
  
-import React from 'react'
-import {Link} from 'react-router-dom'
- 
-const NavBar = () => {
-  return (
-    <div>
-        <nav>
-            <h1>Online Survey and Analysis Platform</h1>
-            <Link to ="/add"><button>Add Survey</button></Link>
-            <Link to  = "/surveys"><button>Surveys</button></Link>
-        </nav>
-    </div>
-  )
-}
- 
-export default NavBar
- 
-surveylist.jxs
- 
-import React from 'react'
-import axios from "axios";
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-const SurveyList = () => {
- 
-    const[Data, SetData] = useState([]);
-    const[msg,setMsg] = useState("");
-    const[err,setErr] = useState("");
-    const nav = useNavigate();
- 
-    const fetchData = async()=>{
-      try{
-        const response = await axios.get("https://8080-bcecbbaaacaed351060496eadcbaafbcaone.premiumproject.examly.io/api/surveys");
-        console.log(response);
-        SetData(response.data);
-      }catch(err){
-          console.log(err);
-      }
-     
-   
-    }
-    useEffect(()=>{
-      const fetch = async()=>{
-          await fetchData();
-      }
-      fetch();
-    },[]);
- 
-    const handleDelete = async(surveyId)=>{
-      try{
-        await axios.delete(`https://8080-bcecbbaaacaed351060496eadcbaafbcaone.premiumproject.examly.io/api/surveys/${surveyId}`);
-        setMsg("Successfully deleted.")
-        await fetchData()
-      }catch{
-          setErr("Error deleting survey")
-      }
- 
-    }
-    if(err) return <p>{err}</p>
-  return (
-    <div>
-      {/* <h1>Online Survey and Analysis Platform</h1> */}
-      {msg &&<p>{msg}</p>}
-      {Data.length===0?(<p>No surveys available</p>):(Data.map((survey)=>(
-      <div className='card key' key={survey.surveyId}>
-          <h3>{survey.title}</h3>
-          <p>{survey.description}</p>
-          <div>
-            <button className='edit' onClick={()=>
-              nav(`/edit/${survey.surveyId}`)
-            }>Edit
-            </button>
- 
-            <button className='delete' onClick={()=>
-              handleDelete(survey.surveyId)
-            }>Delete
-            </button>
-          </div>
-      </div>)))}
- 
-      {/* <label htmlFor="Survey 1">Survey 1</label>
-      <label htmlFor="Survey 2">Survey 2</label> */}
-    </div>
-  )
-}
- 
-export default SurveyList
- 
-program.cs
-using dotnetapp.Data;
-using dotnetapp.Services;
-using Microsoft.EntityFrameworkCore;
- 
-var builder = WebApplication.CreateBuilder(args);
- 
-// Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>(
-    options=>options.UseSqlServer(builder.Configuration.GetConnectionString("myconnstring"))
-);
-builder.Services.AddScoped<SurveyService>();
- 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
- 
- 
-builder.Services.AddCors(options=>options.AddPolicy("AllowFrontend",policy=>{
-    policy
-    .WithOrigins("https://8081-bcecbbaaacaed351060496eadcbaafbcaone.premiumproject.examly.io")
-    .AllowAnyHeader()
-    .AllowAnyMethod();
-}));
- 
-var app = builder.Build();
- 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace dotnetapp.Services
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-app.UseCors("AllowFrontend");
- 
-app.UseHttpsRedirection();
- 
-app.UseAuthorization();
- 
-app.MapControllers();
- 
-app.Run();
- 
-Get started with Swashbuckle and ASP.NET Core | Microsoft Learn
-Learn how to add Swashbuckle to your ASP.NET Core web API project to integrate the Swagger UI.
- 
-surveyform.jsx
- 
-import React, { useState } from 'react'
-import NavBar from './NavBar'
-import axios from "axios";
-import { useParams } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
- 
-const SurveyForm = ({mode}) => {
-  const[formData, setFormData]= useState({
-    title:"",
-    description:""
-  })
-  const handleChange =(e)=>{
-    const {name,value} = e.target;
-    setFormData({...formData, [name]:value})
-  }
-  const[isSub, setIsSub] = useState(false);
-  const[error,setError] = useState({});
-  const [msg,setMsg] = useState("");
-  const nav = useNavigate();
- 
-  const validation = ()=>{
-    let errors = {};
-    if(!formData.title.trim())
+    public class BooksService // Class for Business logic and DBOps
     {
-      errors.title = "Title is required"
+        private static string connectionString = ConfigurationManager.ConnectionStrings["myconnstring"].ConnectionString;
+ 
+        public static void AddBook(List<Book> books, int libraryId, string title, string author, string category, decimal price)
+        {
+            int nextId = books.Count == 0 ? 1 : books[books.Count - 1].Id + 1; // Ternary Operator (? :), short-hand if-else; 0: assign 1, else: last ID + 1
+            books.Add(new Book
+            {
+                Id = nextId,
+                Title = title,
+                Author = author,
+                Category = category,
+                Price = price,
+                LibraryId = libraryId
+ 
+            });
+            Console.WriteLine("Book added successfully!");
+        }
+ 
+        public static void ListBooks(List<Book> books, List<Library> libraries) // Spec demands List<Library> libraries to be here
+        {
+            if(books == null || books.Count == 0)
+            {
+                Console.WriteLine("No Books available.");
+                return;
+            }
+            Console.WriteLine("List of Books:");
+            foreach(Book b in books)
+            {
+                Console.WriteLine($"Book ID: {b.Id}, Title: {b.Title}, Author: {b.Author}, Category: {b.Category}, Price: ${b.Price}");
+            }
+        }
+ 
+        public static void FindBook(List<Book> books, string bookTitle)
+        {
+            Book b = books.Find(x=>x.Title == bookTitle); // x => is x tends to; => is a Lambda expression; We are using the Find() Function here.
+            if(b == null)
+            {
+                Console.WriteLine("Book not found.");
+                return;
+            }
+ 
+            Console.WriteLine($"Book ID: {b.Id}, Title: {b.Title}, Author: {b.Author}, Category: {b.Category}, Price: ${b.Price}");
+               
+        }
+ 
+        public static void EditBook(List<Book> books, int id, string title, string author, string category, string price)
+        {
+            Book b = books.Find(x=>x.Id == id);
+            if(b == null)
+            {
+                Console.WriteLine("Book not found.");
+                return;
+            }
+ 
+            if(!string.IsNullOrEmpty(title)) b.Title = title;
+            if(!string.IsNullOrEmpty(author)) b.Author = author;
+            if(!string.IsNullOrEmpty(category)) b.Category = category;
+            if(!string.IsNullOrEmpty(price)) b.Price = decimal.Parse(price);
+ 
+            Console.WriteLine("Book information updated successfully!");
+   
+        }
+ 
+        public static void DeleteBook(List<Book> books, int id)
+        {
+            Book b = books.Find(x=>x.Id==id);
+            if(b==null)
+            {
+                Console.WriteLine("Book not found.");
+                return;
+            }
+            books.Remove(b);
+            Console.WriteLine("Book deleted successfully!");
+               
+        }
+ 
+        public static void AddBookToDB(int libraryId, string title, string author, string category, decimal price)
+        {
+            try
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Books", connectionString);
+                SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+ 
+                DataRow row = table.NewRow();
+                row["Title"] = title;
+                row["Author"] = author;
+                row["Category"] = category;
+                row["Price"] = price;
+                row["LibraryId"] = libraryId;
+                table.Rows.Add(row);
+ 
+                adapter.Update(table);
+                Console.WriteLine("Book added to the database successfully!");
+ 
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Database connection failed.");
+            }
+               
+        }
+ 
+        public static void ListBooksFromDB()
+        {  
+            try
+            {
+                string query = "SELECT b.Id, b.Title, b.Author, b.Category, b.Price, l.Name AS LibraryName FROM Books b INNER JOIN Libraries l ON b.LibraryId = l.Id";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connectionString);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+ 
+                foreach(DataRow row in table.Rows)
+                {
+                    Console.WriteLine($"Book ID: {row["Id"]}, Title: {row["Title"]}, Author: {row["Author"]}, Category: {row["Category"]}, Price: ${row["Price"]}, Library: {row["LibraryName"]}");
+                }
+                   
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Database connection failed.");
+            }
+        }
+ 
+        public static void EditBookInDB(int id, string title, string author, string category, string price)
+        {   try
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter($"SELECT * FROM Books WHERE Id = {id}", connectionString);
+                SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+ 
+                if(table.Rows.Count == 0)
+                {
+                    Console.WriteLine("Book not found.");
+                    return;
+                }  
+ 
+                DataRow row = table.Rows[0];
+ 
+                if(!string.IsNullOrEmpty(title)) row["Title"] = title;
+                if(!string.IsNullOrEmpty(author)) row["Author"] = author;
+                if(!string.IsNullOrEmpty(category)) row["Category"] = category;
+                if(!string.IsNullOrEmpty(price)) row["Price"] = decimal.Parse(price);
+ 
+                adapter.Update(table);
+                Console.WriteLine("Book updated successfully!");
+ 
+            }
+ 
+            catch(Exception)
+            {
+                Console.WriteLine("Database connection failed.");
+            }
+           
+        }
+ 
+        public static void DeleteBookFromDB(int id)
+        {
+            try
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter($"SELECT * FROM Books WHERE Id = {id}", connectionString);
+                SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+ 
+                if(table.Rows.Count == 0)
+                {
+                    Console.WriteLine("Book not found.");
+                    return;
+                }
+ 
+                table.Rows[0].Delete();
+                adapter.Update(table);
+                Console.WriteLine("Book deleted successfully from the database!");
+ 
+            }
+ 
+            catch(Exception)
+            {
+                Console.WriteLine("Database connection failed.");
+ 
+            }
+        }
+       
+ 
     }
-    if(!formData.description.trim())
-    {
-      errors.description = "Description is required"
-    }
-    return errors;
-  }
-  const handleSubmit = async(e)=>{
-    e.preventDefault();
-    let allError = validation();
-    setError(allError);
-    if(Object.keys(allError).length===0)
-    {
-      if(mode==="add"){
-        await send()
-        setMsg("Survey added successfully!")
-      }
-      else{
-        await update();
-        setMsg("Survey updated successfully!")
-      }
-      setIsSub(true);
- 
-      setFormData({
-        title:"",
-        description:""
-      })
- 
-      setTimeout(()=>{
-        setIsSub(false);
-        nav("/")
- 
-      },3000)
-    }
-  }
-  const send  =  async()=>{
-    try{
-      const res =  await axios
-      .post
-      ("https://8080-bcecbbaaacaed351060496eadcbaafbcaone.premiumproject.examly.io/api/surveys",{
-        title:formData.title,
-        description:formData.description
-      });
-      console.log(res.data);
-    }catch(error){
-      console.error(error);
-    }
-  }
-  const{id} = useParams();
- 
-  const update = async()=>{
-    try{
-      const res =  
-      await axios
-      .put
-      (`https://8080-bcecbbaaacaed351060496eadcbaafbcaone.premiumproject.examly.io/api/surveys/${id}`,{
-        title:formData.title,
-        description:formData.description
-      })
-      console.log(res.data);
-    }catch(error){
-      console.error(error);
-    }
-  }
- 
-  return (
-    <div>
-        <button type="button" onClick={()=>nav("/")}>Back</button>
- 
-        <form onSubmit={handleSubmit}>
-          <h2>{mode==="add"?"Add Survey":"Edit Survey"}</h2>
-        <div>
-            <input type="text" placeholder="Survey Title" name='title' value={formData.title} onChange={handleChange}/>
-            {error.title&& <p>{error.title}</p>}
-        </div>
-        <div>
-          <textarea placeholder='Survey Description' name='description' value={formData.description} onChange={handleChange}></textarea>
-          {error.description && <p>{error.description}</p>}
-        </div>
-        <button type='submit'className='sucess' >{mode==="add"?"Save":"Update"}</button>
-        </form>
-        {msg && <p>{msg}</p>}
-    </div>
-  )
 }
  
-export default SurveyForm
-
-app.js
-import './App.css';
-import NavBar from './Components/NavBar';
-import SurveyForm from './Components/SurveyForm';
-import SurveyList from './Components/SurveyList';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
  
-function App() {
-  return (
-    <div >
-      <Router>
-        <NavBar/>
-        <Routes>
-          <Route path='/add' element={<SurveyForm mode="add"/>}></Route>
-          <Route path='/surveys' element={<SurveyList/>}></Route>
-          <Route path='/edit/:id' element={<SurveyForm mode="edit"/>}></Route>        
-        </Routes>
-      </Router>
-     
-    </div>
-  );
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using dotnetapp.Models;
+ 
+namespace dotnetapp.Services
+{
+    public class LibrariesService // Class for Business logic and DBOps
+    {
+        private static string connectionString = ConfigurationManager.ConnectionStrings["myconnstring"].ConnectionString;
+        public static void AddLibrary(List<Library> libraries, string name, string address, int maximumCapacity)
+        {
+            int nextId = libraries.Count == 0 ? 1 : libraries[libraries.Count - 1].Id + 1;
+            libraries.Add(new Library
+            {
+                Id = nextId,
+                Name = name,
+                Address = address,
+                MaximumCapacity = maximumCapacity
+ 
+            });
+ 
+            Console.WriteLine("Library added successfully!");
+        }
+ 
+        public static void ListLibraries(List<Library> libraries)
+        {
+            if(libraries==null || libraries.Count == 0)
+            {  
+                Console.WriteLine("No Libraries available.");
+                return;
+            }
+ 
+            Console.WriteLine("List of Libraries:");
+ 
+            foreach(Library l in libraries)
+            {
+               
+                Console.WriteLine($"Library ID: {l.Id}, Name: {l.Name}, Address: {l.Address}, Capacity: {l.MaximumCapacity}");
+               
+            }
+       
+        }
+ 
+        public static void AddLibraryToDB(string name, string address, int maximumCapacity)
+        {
+            try
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Libraries", connectionString);
+                SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+ 
+               
+                DataRow row = table.NewRow();
+                row["Name"] = name;
+                row["Address"] = address;
+                row["MaximumCapacity"] = maximumCapacity;
+                table.Rows.Add(row);
+ 
+                adapter.Update(table);
+                Console.WriteLine("Library added to the database successfully!");
+            }
+ 
+            catch(Exception)
+            {
+                Console.WriteLine("Database connection failed.");
+ 
+            }
+        }
+ 
+        public static void ListLibrariesFromDB()
+        {
+            try
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Libraries", connectionString);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+ 
+                foreach(DataRow row in table.Rows)
+                {
+                    Console.WriteLine($"Library ID: {row["Id"]}, Name: {row["Name"]}, Address: {row["Address"]}, Capacity: {row["MaximumCapacity"]}");
+                }
+                   
+            }
+           
+            catch(Exception)
+            {
+                Console.WriteLine("Database connection failed.");
+            }
+        }
+ 
+ 
+    }
 }
  
-export default App;                           
-
-
-                           
+ 
+namespace dotnetapp.Managers
+{
+    public interface IBookManager // Interface for DIP. Any method declared here but me implemented or defined in any class that implements this interface.
+    {
+        void AddBook(int libraryId);
+        void ListBooks();
+        void FindBook(string bookTitle);
+        void EditBook();
+        void DeleteBook();
+        void AddBookToDB(int libraryId);
+        void ListBooksFromDB();
+        void EditBookInDB();
+        void DeleteBookFromDB();
+ 
+    }
+}
+ 
+namespace dotnetapp.Managers
+{
+    public interface ILibraryManager  // Two interfaces for Interface Segregation Principle
+    {
+        void AddLibrary();
+        void ListLibraries();
+        void AddLibraryToDB();
+        void ListLibrariesFromDB();
+       
+    }
+}
  
